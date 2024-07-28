18886e2225d80f910b7cd67dc189f35ad0ed7445
Vlad <vadya-85@mail.ru>
2024-07-28 18:01:59 +0200

    Update ContactElement.jsx

--- src/components/ContactElemet/ContactElement.jsx
+++ src/components/ContactElemet/ContactElement.jsx
@@ -2,12 +2,10 @@ import { useDispatch } from 'react-redux';
 import { Button, Div, ListItem } from './ContactElement.styled';
 import { deleteContact } from 'redux_files/contacts/operations';
 
-
-export const ContactElement = ({ id, name, phone}) => {
+export const ContactElement = ({ id, name, phone }) => {
   const dispatch = useDispatch();
-  const handleDelete = () => {
-    dispatch(deleteContact(id))
-  }
+  const handleDelete = () => dispatch(deleteContact(id));
+
   return (
     <ListItem>
       <Div>
